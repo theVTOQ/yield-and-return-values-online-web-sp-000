1ff2ec2f1e96fd4ebe7c31b507f6c587d895b051
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-11-11 17:06:44 +0000

    automatically backed up by learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -3,8 +3,8 @@ require 'pry'
 def hello(array)
   i = 0
   while i < array.length
-    binding.pry
-    yield(array[i])
+    collection = []
+    collection << yield(array[i])
     i += 1
   end
 end
